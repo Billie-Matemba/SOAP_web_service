5c430c870f6b5bedf2d50a962a8d006076fb2a4f
Billie-Matemba <billiematemba133@gmail.com>
2025-12-01 22:37:42 +0200

    deploy

--- meter_project/wsgi.py
+++ meter_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meter_project.meter_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
